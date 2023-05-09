a42582f50b25b280011b2a256f7cb32d32c3d049
nastyazz <113694867+nastyazz@users.noreply.github.com>
2023-05-09 08:44:12 +0300

    Add files via upload

---
+++ dz.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    echo "Введите имя файла в качестве аргумента"
+    exit 1
+fi
+
+input_file=$1
+encrypted_file="$input_file.enc"
+key_file="$input_file.key"
+
+# Если ключ для расшифровки не был передан как аргумент, шифруем файл и сохраняем ключ
+if [[ $# -eq 1 ]]; then
+    md5sum "$input_file" | cut -d' ' -f1 > "$key_file"
+    openssl enc -aes-256-cbc -salt -in "$input_file" -out "$encrypted_file"
+    echo "Файл '$input_file' успешно зашифрован и сохранен в '$encrypted_file'"
+    echo "Ключ для расшифровки сохранен в '$key_file'"
+    exit 0
+fi
+
+# Иначе пытаемся расшифровать файл с переданным ключом
+if [[ $# -eq 2 ]]; then
+    key=$2
+else
+    key=$(cat "$key_file")
+fi
+
+if ! openssl enc -d -aes-256-cbc -salt -in "$encrypted_file" -out "$input_file" -pass "pass:$key" 2>/dev/null; then
+    echo "Неверный ключ для расшифровки или файл '$encrypted_file' не существует"
+    exit 1
+fi
+
+echo "Файл '$encrypted_file' успешно расшифрован и сохранен в '$input_file'"
